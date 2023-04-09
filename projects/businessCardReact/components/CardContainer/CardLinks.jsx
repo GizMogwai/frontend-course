1604437abda0a8d825926e18101551ad30f1ec3d
0xGizmo <james.jager94@gmail.com>
2023-04-09 17:57:44 +0400

    chore: Update cardLinks component

--- projects/businessCardReact/components/CardContainer/CardLinks.jsx
+++ projects/businessCardReact/components/CardContainer/CardLinks.jsx
@@ -10,22 +10,16 @@ function CardLinks() {
       bgColor="#161619"
       borderBottomRadius="10px"
     >
-      <Link
-        href="#"
-        _hover={{
-          filter:
-            "brightness(0) saturate(100%) invert(98%) sepia(53%) saturate(6846%) hue-rotate(296deg) brightness(108%) contrast(101%)",
-        }}
-      >
+      <Link href="#">
         <InstagramIcon />
       </Link>
-      <Link href="3">
+      <Link href="#">
         <GitHubIcon />
       </Link>
-      <Link href="3">
+      <Link href="#">
         <FacebookIcon />
       </Link>
-      <Link href="3">
+      <Link href="#">
         <TwitterIcon />
       </Link>
     </CardFooter>
